ef84342f8586a444aa48ba0d5899e8c40cbf88d1
Shubhi Dwivedi <shubhidwivedi1415@gmail.com>
2025-04-22 10:17:05 +0530

    setting expess

---
+++ expressDir/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+
+let port = 8080;
+
+app.listen(port , () => {
+    console.log(`app listening on port ${port}`);
+});
+
+// app.use((req,res)=> {
+//     console.log("request recieved");
+//     res.send("this is our response");
+// });
+
+app.get("/" , (req,res)=> {
+    res.send("you contacted root path");
+});
+
+app.get("/search",(req , res) => {
+    res.send("you contacted search path");
+});
+
+app.get("*" , (req,res) => {
+    res.send("this path does not exist");
+});
