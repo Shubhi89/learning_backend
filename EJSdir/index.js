9cbe0c5fb99bfee55a4139ea26624955aed07295
Shubhi Dwivedi <shubhidwivedi1415@gmail.com>
2025-04-23 10:40:26 +0530

    using EJS

---
+++ EJSdir/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = 8080;
+
+// using ejs
+app.set("view engine" , "ejs");
+app.set("views" , path.join(__dirname , "/views"));
+
+app.get("/" , (req , res) => {
+    res.render("home.ejs");
+});
+
+app.listen(port , ()=> {
+    console.log(`listening using port ${port}`);
+});
+
