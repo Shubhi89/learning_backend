497e0be3fc2dc25b5619f8e9173a8f9a94a02810
Shubhi Dwivedi <shubhidwivedi1415@gmail.com>
2025-05-09 10:43:22 +0530

    forming connection

--- mongoDir/index.js
+++ mongoDir/index.js
@@ -1,2 +1,10 @@
 const mongoose = require("mongoose");
-mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+main().then(() => {
+    console.log("connection successfull");
+})
+.catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+}
