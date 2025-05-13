d7e41d16399980b0ee1ec931d2562fe95fe12f67
Shubhi Dwivedi <shubhidwivedi1415@gmail.com>
2025-05-13 09:50:24 +0530

    using Date

--- mongoDir/main.js
+++ mongoDir/main.js
@@ -7,6 +7,8 @@ const Chat = require("./models/chat.js");
 app.set("views" ,path.join(__dirname , "views"));
 app.set("view engine" , "ejs");
 app.use(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded({extended:true}));
+
 main().then(() => {
     console.log("connection successfull");
 })
@@ -25,6 +27,20 @@ app.get("/chats" , async (req,res)=> {
 // new route
 app.get("/chats/new" , (req , res)=> {
    res.render("new.ejs"); 
+});
+
+// create route
+app.post("/chats" , (req,res)=> {
+    let {from , to , msg} = req.body;
+    let newChat = new Chat({
+        from: from,
+        to: to,
+        msg: msg,
+        created_at: new Date()
+    });
+    
+    newChat.save();
+    res.redirect("/chats");
 });
 
 app.get("/" , (req , res) => {
