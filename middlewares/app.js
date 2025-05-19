27bee05afabc06ad2d89fa68abf8f3d47021f794
Shubhi Dwivedi <shubhidwivedi1415@gmail.com>
2025-05-19 10:34:27 +0530

    api token as query string

--- middlewares/app.js
+++ middlewares/app.js
@@ -1,4 +1,3 @@
-// index.js
 const express = require('express');
 const app = express();
 const PORT = 8080;
@@ -6,6 +5,18 @@ const PORT = 8080;
 app.use((req,res,next) => {
     console.log(req);
     next();
+});
+
+app.use("/api", (req,res,next) => {
+    let {token} =  req.query;
+    if(token === "giveaccess") {
+        next();
+    }
+    res.send("Access Denied");
+});
+
+app.get("/api", (req, res) => {
+    res.send("Access Granted");
 });
 
 app.get('/', (req, res) => {
